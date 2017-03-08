131cdba308229c9e64cca5c93ef689f95874a657
Kenneth Galle <ken.galle@rainshowers.org>
2017-03-07 21:51:00 -0500

    working limited version of enum

--- enum.go
+++ enum.go
@@ -1,5 +1,7 @@
 package util
 
+import "errors"
+
 // type enummap map[string]int
 
 type Enumbase struct {
@@ -10,19 +12,38 @@ type Enumbase struct {
 
 // this needs to be duplicated for each enum type
 func NewEnumbase(keys []int, current int) *Enumbase {
-	enum := new(EnumOne)
+	enum := new(Enumbase)
+	enum.Init(keys, current)
+	return enum
+}
+
+func (enum *Enumbase) Init(keys []int, current int) *Enumbase {
 	enum.keys = make(map[int]interface{})
 	for _, val := range keys {
 		enum.keys[val] = nil
 	}
-	// TODO store the key/index of the current value passed in
 	enum.currentkey = current
 	return enum
 }
 
 func (enum *Enumbase) Clone() *Enumbase {
-    newenum := NewEnumbase(enum.keys, enum.currentkey)
-    return newenum
+	// http://stackoverflow.com/a/27848197/3728147
+	keys := make([]int, len(enum.keys))
+	i := 0
+	for k := range enum.keys {
+		keys[i] = k
+		i++
+	}
+	newenum := NewEnumbase(keys, enum.currentkey)
+	return newenum
 }
 
-func (enum *Enumbase) Clone() *Enumbase {
+func (enum *Enumbase) Set(current int) (*Enumbase, error) {
+	_, ok := enum.keys[current]
+	if !ok {
+		return nil, errors.New("invalid enum value")
+	} else {
+		enum.currentkey = current
+		return enum, nil
+	}
+}
--- enum_test.go
+++ enum_test.go
@@ -12,15 +12,15 @@ type EnumOne struct {
 
 // this needs to be duplicated for each enum type
 func NewEnumOne(keys []int, current int) *EnumOne {
-	enum := NewEnumbase(keys, current)
+	enum := new(EnumOne)
+	enum.Init(keys, current)
 	return enum
 }
 
 func TestEnum(t *testing.T) {
 	e := NewEnumOne([]int{1, 2, 3}, 2)
 	fmt.Printf("%v\n", e)
-}
-
-func DoSomething(e EnumOne) {
-	fmt.Printf("%v\n", e)
+	f := e.Clone()
+	f.Set(1)
+	fmt.Printf("%v\n", f)
 }
