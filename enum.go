c0b5cb8f815b67f5aefe0cd30c31ecb78107ae8d
Kenneth Galle <ken.galle@rainshowers.org>
2017-03-08 17:15:57 -0500

    add comments for enum

--- enum.go
+++ enum.go
@@ -1,3 +1,6 @@
+// The enum package provides a minimal version of an enum type, in a way similar
+// to what is found in Java.  Each enum is a separate type and so can prevent
+// type errors, ie. two enums that contain the same element will not be equal.
 package util
 
 import (
@@ -6,11 +9,24 @@ import (
 	"reflect"
 )
 
+// https://gist.github.com/skarllot/102a5e5ea73861ff5afe
+// http://stackoverflow.com/a/28393465/3728147
+
+// Enumbase defines the operations available within an enum.
 type Enumbase interface {
+	// Initialize a newly created enum with the supplied values.
+	// keys is a list of elements that are legal values in the enum.
+	// current is the one element that exists in keys that is the enum's current
+	// value.
 	Init(keys []int, current int)
+	// Clone creates a new enum with the same keys and current value as this.
 	Clone() *EnumbaseImpl
+	// Set changes the current value of the enum to a new value that is within keys.
 	Set(current int) error
+	// Equal tests when this enum equals the suppied other enum.  The keys and
+	// the current value must match.
 	Equal(other Enumbase) bool
+	// Print prints to standard output the content of this enum.
 	Print()
 }
 
@@ -58,6 +74,7 @@ func (enum *EnumbaseImpl) Set(current int) error {
 
 func (enum *EnumbaseImpl) Equal(other Enumbase) bool {
 	otherbase := other.(*EnumbaseImpl)
+	// http://stackoverflow.com/a/27160765/3728147
 	typesame := reflect.TypeOf(enum) == reflect.TypeOf(otherbase)
 	valuesame := enum.currentkey == otherbase.currentkey
 	return typesame && valuesame
